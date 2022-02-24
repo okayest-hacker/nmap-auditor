cd727707e80ecf5be57099ee1005ec23fea8d52d
OkayestHacker <26881768+okayest-hacker@users.noreply.github.com>
2022-02-23 20:16:41 -0500

    Update nmap-auditor.py

--- nmap-auditor.py
+++ nmap-auditor.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/python -tt
 # -*- coding: utf-8 -*-
 #must pip3 install python-libnmap
 from libnmap.process import NmapProcess
 from libnmap.parser import NmapParser, NmapParserException
+import re
 
 def do_scan(targets, options):
     parsed = None
-    nmproc = NmapProcess(targets, options)
+    nmproc = NmapProcess(targets,options)
     rc = nmproc.run()
     if rc != 0:
         print("nmap scan failed: {0}".format(nmproc.stderr))
-    print(type(nmproc.stdout))
-
     try:
         parsed = NmapParser.parse(nmproc.stdout)
     except NmapParserException as e:
@@ -22,26 +21,21 @@ def do_scan(targets, options):
 
 # print scan results from a nmap report
 def print_scan(nmap_report):
-    print(key, "has been run")
+    print(key, '-', command2)
     f.write("--------------------------------------------------------------------------" +"\n")
     f.write(str(key) + "\n")
     f.write("--------------------------------------------------------------------------" +"\n")
-    print("Starting Nmap {0} ( http://nmap.org ) at {1}".format(
-        nmap_report.version,
-        nmap_report.started))
-
     for host in nmap_report.hosts:
         if len(host.hostnames):
             tmp_host = host.hostnames.pop()
         else:
             tmp_host = host.address
-        print("Nmap scan report for {0} ({1})".format(
-            tmp_host,
-            host.address))
-        print("Host is {0}.".format(host.status))
-        f.write("Host is {0}.".format(host.status) + "\n")
+        print("----------------------------------------------------------")
+        f.write("------------------------------------------------------" + "\n")
         print("  PORT     STATE         SERVICE")
         f.write("  PORT     STATE         SERVICE" + "\n")
+        print("-----------------------------------------------------------")
+        f.write("-----------------------------------------------------------" + "\n")
 
         for serv in host.services:
             pserv = "{0:>5s}/{1:3s}  {2:12s}  {3}".format(
@@ -53,26 +47,24 @@ def print_scan(nmap_report):
                 pserv += " ({0})".format(serv.banner)   
             print(pserv)
             f.write(pserv + "\n")
-    print(nmap_report.summary)
 if __name__ == "__main__":
-        scantarget = input('what ip address do you want to scan: ')
-        portz = input('what ports do you want to scan(example:1-100, 500): ')
+        scantarget = input('what ip address do you want to scan(use the -6 option before an ivp6 address): ')
+        portz = input('what ports do you want to scan(example:1-100, 500 or - for all ports): ')
         f = open('%s.txt' % scantarget,'w')
-        #just add scan variables here like "'FIN':'-sF'" or "-sS"
-        scantypes = {'FIN':'-sF','WINDOW':'-sW','SYN':'-sS'}
+        #just add scan variables here like 'MAIMON':'-sM','PROTOCOL':'-sO'
+        scantypes = {'MAIMON':'-sM','PROTOCOL':'-sO'}
         valuez = scantypes.values()
         keyz = scantypes.keys()
         for key, value in scantypes.items():
-                command = ( scantarget, value,'-p'+ portz, '--open')
-                commandz = ''.join(str(command))
-                command1 = commandz.replace(",", "")
-                command2 = command1.replace("'", "")
-                command3 = command2.replace("(", "")
-                command4 = command3.replace(")", "")
-                command5 = command4.strip()
-                report = do_scan(value,command5)
-                if report:
-                        print_scan(report) 
-                else:
-                        print("No results returned")
+            if value == '-sO':
+                portz = '1-255'
+            command = (scantarget,value,'-p'+ portz)
+            commandz = ''.join(str(command))
+            command1 = re.sub(r"[^a-zA-Z0-9-. ]", "", commandz)
+            command2 = str(command1)
+            report = do_scan(" ", command2)
+            if report:
+                print_scan(report) 
+            else:
+                print("No results returned")
 f.close()
