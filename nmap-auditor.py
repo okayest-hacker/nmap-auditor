1074230cc14787ef7dfc5db346bec19dd185b623
OkayestHacker <26881768+okayest-hacker@users.noreply.github.com>
2022-02-24 20:40:41 -0500

    Update nmap-auditor.py

--- nmap-auditor.py
+++ nmap-auditor.py
@@ -3,6 +3,7 @@
 #must pip3 install python-libnmap
 import re
 import os, sys
+import questionary
 from colorama import Fore, Back, Style, init
 from libnmap.process import NmapProcess
 from libnmap.parser import NmapParser, NmapParserException
@@ -24,22 +25,20 @@ def do_scan(targets, options):
 # print scan results from a nmap report
 def print_scan(nmap_report):
     print("----------------------------------------------------------")
-    print(Fore.GREEN + key, command2 + Fore.RESET)
-    f.write("--------------------------------------------------------------------------" +"\n")
-    f.write(str(key) + "\n")
-    f.write("--------------------------------------------------------------------------" +"\n")
+    print(Fore.GREEN,command2 + Fore.RESET)
+    f.write("----------------------------------------------------------" +"\n")
+    f.write(str(command2) + "\n")
     for host in nmap_report.hosts:
         if len(host.hostnames):
             tmp_host = host.hostnames.pop()
         else:
             tmp_host = host.address
         print("----------------------------------------------------------")
-        f.write("------------------------------------------------------" + "\n")
+        f.write("----------------------------------------------------------" + "\n")
         print("  PORT     STATE         SERVICE")
         f.write("  PORT     STATE         SERVICE" + "\n")
-        print("-----------------------------------------------------------")
-        f.write("-----------------------------------------------------------" + "\n")
-
+        print("----------------------------------------------------------")
+        f.write("----------------------------------------------------------" + "\n")
         for serv in host.services:
             pserv = "{0:>5s}/{1:3s}  {2:12s}  {3}".format(
                     str(serv.port),
@@ -53,17 +52,23 @@ def print_scan(nmap_report):
 if __name__ == "__main__":
     if not os.geteuid()==0:
         sys.exit(Fore.RED + 'This script must be run as root!')
-    scantarget = input('what ip address do you want to scan(use the -6 option before an ivp6 address): ')
+    IPv4 = "IPv4"
+    IPv6 = "Ipv6"
+    test1 = questionary.select("do you want to scan IPv4 or IPV6",choices=[IPv4, IPv6],).ask()
+    IPV6var = ""
+    if test1 == IPv6:
+        test1 = '-6'
+    elif test1 == IPv4:
+        test1 = ""
+    scantarget = input('Please enter an ip address: ')
+    scantypez = questionary.checkbox('Select scan type or types;', choices=['-sS','-sT','-sF','-sX','-sU','-sW','-sM','-sO']).ask()
+    print(scantypez)
     portz = input('what ports do you want to scan(example:1-100, 500 or - for all ports): ')
     f = open('%s.txt' % scantarget,'w')
-    #just add scan variables here like 'MAIMON':'-sM','PROTOCOL':'-sO'
-    scantypes = {'MAIMON':'-sM','PROTOCOL':'-sO'}
-    valuez = scantypes.values()
-    keyz = scantypes.keys()
-    for key, value in scantypes.items():
-        if value == '-sO':
+    for x in scantypez:
+        if x == '-sO':
             portz = '1-255'
-        command = (scantarget,value,'-p'+ portz)
+        command = (scantarget,test1,x,'-p'+ portz)
         commandz = ''.join(str(command))
         command1 = re.sub(r"[^a-zA-Z0-9-. ]", "", commandz)
         command2 = str(command1)
